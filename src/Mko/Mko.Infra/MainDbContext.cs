4750dbe10395d5fe813a7d11398a3b54ddf0dd40
olegor2003 <olegor2003@mail.ru>
2017-10-24 22:33:51 +0300

    Ьщвуды

---
+++ src/Mko/Mko.Infra/MainDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mko.Infra
+{
+    public class MainDbContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Mko/Mko.ObjectModel/Model/Grade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mko.ObjectModel.Model
+{
+    public class Grade
+    {
+        public int Id { get; set; }
+
+        public string Letter { get; set; }
+
+        public int Parallel { get; set; }
+    }
+}
